db01e03f4e0f84e0aa6b604ffb18fad90b7c890b
Jason Scott <jason@jasonscott.io>
2017-04-23 21:23:30 +0200

    util

--- server/server.js
+++ server/server.js
@@ -6,28 +6,47 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const pug = require('pug');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+
 
 let app = express();
+
+const {generateMessage} = require('./utils/message');
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
+app.use('public/js', express.static(path.join(__dirname, '/public/js')));
+app.use('public/css', express.static(path.join(__dirname, '/public/css')));
+
+
+
+
 let server = http.createServer(app);
 let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Test Pug Page'
+    })
+});
+
+
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined the channel'));
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
     socket.on('disconnect', () => {
